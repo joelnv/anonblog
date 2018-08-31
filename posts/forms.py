38eed7af51eae505b1a44f10a6de78c5d34ef414
joelnv <joel@nuventure.in>
2018-08-31 16:01:59 +0530

    Made changes to forms by reusing existiong form

--- posts/forms.py
+++ posts/forms.py
@@ -3,18 +3,9 @@ from . import models
 
 
 class CreatePosts(forms.ModelForm):
-    class Meta:
-        model = models.Post
-        fields = ['title','body']
-
-    def clean(self):
-        super().clean()
-        title = self.cleaned_data.get('title')
-        body = self.cleaned_data.get('body')
-        if len(title) > len(body):
-            raise forms.ValidationError("body should be longer than title")
-
-class UpdatePosts(forms.ModelForm):
+    forms.error_css_class = "alert alert-danger"
+    title = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Title'}))
+    body = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Body'}))
     class Meta:
         model = models.Post
         fields = ['title','body']
