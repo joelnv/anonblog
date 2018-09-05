037f6d9780232694db86d68323128c54f01ea499
joelnv <joel@nuventure.in>
2018-09-05 17:23:31 +0530

    Made design changes in html files based only on bootstrap 4 , converted  validation error from bootstrap 3 to bootstrap 4 , made changes in forms.py for validation state based on bootstrap 4

--- posts/forms.py
+++ posts/forms.py
@@ -2,10 +2,12 @@ from django import forms
 from . import models
 
 
+valid_state = 'is-valid'
 class CreatePosts(forms.ModelForm):
+
     forms.error_css_class = "alert alert-danger"
-    title = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Title'}))
-    body = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Body'}))
+    title = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control ', 'placeholder': 'Title' ,'pattern':'.{10,}'}))
+    body = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Body' ,'minlength':'10'}))
     class Meta:
         model = models.Post
         fields = ['title','body']
@@ -16,3 +18,7 @@ class CreatePosts(forms.ModelForm):
         body = self.cleaned_data.get('body')
         if len(title) > len(body):
             raise forms.ValidationError("body should be longer than title")
+
+    def clean_title(self):
+        valid_state = 'is-invalid'
+        return self.cleaned_data['title']
